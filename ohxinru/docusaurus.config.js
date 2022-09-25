d34915abe7679ea98e31522da5f87386c524d733
XR <362988082@qq.com>
2022-09-25 22:55:42 +0800

    Update docusaurus.config.js

--- ohxinru/docusaurus.config.js
+++ ohxinru/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ohxinru', // Usually your GitHub org/user name.
   projectName: 'ohxinru.github.io', // Usually your repo name.
-  deploymentBranch: 'main', 
+  deploymentBranch: 'gh-pages', 
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
