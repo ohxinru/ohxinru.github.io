5dc1a53dfce1cd5065ccb547a12a1dce6dd5215f
XR <362988082@qq.com>
2022-09-26 10:35:06 +0800

    init: docusaurus (#4)
    * chore: ci build
    
    * Update docusaurus.config.js
    
    * init: docusaurus

--- ohxinru/docusaurus.config.js
+++ ohxinru/docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ohxinru',
-  tagline: 'Welcome to my blog',
-  url: 'https://ohxinru.github.io/',
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
